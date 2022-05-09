d894b51e71c5661f1e144f6a8031a743d17bd796
Youdiie <youjin@acon3d.com>
2022-05-09 13:53:02 +0900

    UpdateLauncher 함수 내 주석 추가

--- launcher_abler/UpdateLauncher.py
+++ launcher_abler/UpdateLauncher.py
@@ -32,6 +32,8 @@ logger = logging.getLogger()
 
 
 def check_launcher(dir_, launcher_installed) -> Tuple[Enum, Optional[list]]:
+    """최신 릴리즈가 있는지 URL 주소로 확인"""
+
     finallist = None
     results = []
     state_ui = None
@@ -69,7 +71,7 @@ def check_launcher(dir_, launcher_installed) -> Tuple[Enum, Optional[list]]:
 
 
 def get_req_from_url(url, state_ui, launcher_installed, dir_) -> Tuple[bool,Optional[dict],Enum,str]:
-    # 깃헙 서버에서 url의 릴리즈 정보를 받아오는 함수
+    """깃헙 서버에서 url의 릴리즈 정보를 받아오는 함수"""
 
     # Do path settings save here, in case user has manually edited it
     config = configparser.ConfigParser()
@@ -105,7 +107,8 @@ def get_req_from_url(url, state_ui, launcher_installed, dir_) -> Tuple[bool,Opti
 
 
 def get_results_from_req(req, results) -> None:
-    # req에서 필요한 info를 results에 추가
+    """req에서 필요한 info를 results에 추가"""
+
     for asset in req["assets"]:
         target = asset["browser_download_url"]
         filename = target.split("/")[-1]
