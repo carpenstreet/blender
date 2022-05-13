4d871649a2a571fddb3363ad59f5fc4516dd9808
Youdiie <faengiman@gmail.com>
2022-05-13 16:26:19 +0900

    팝업 영문 수정

--- release/scripts/startup/abler/credential_modal.py
+++ release/scripts/startup/abler/credential_modal.py
@@ -359,7 +359,7 @@ class Acon3dAnchorOperator(bpy.types.Operator):
 
 class Acon3dVersionPopupOperator(bpy.types.Operator):
     bl_idname = "acon3d.version_popup"
-    bl_label = "[에이블러 0.2.2버전 업데이트 안내]"
+    bl_label = "[MANDATORY UPDATE]"
     bl_translation_context = "*"
 
     def execute(self, context):
@@ -371,17 +371,25 @@ class Acon3dVersionPopupOperator(bpy.types.Operator):
     def draw(self, context):
         layout = self.layout
         layout.label(text="")
-        layout.label(text="에이블러는 더 나은 서비스를 위하여 오는 16일 0.2.2버전으로의 업데이트를 제공합니다.")
-        layout.label(text="이후, 0.2.1버전은 지원이 중단 될 예정입니다.")
+        layout.label(
+            text="The new version of ABLER will be released on May 16th, 2022 (KST)."
+        )
+        layout.label(
+            text="Please click the button below to update to the latest version."
+        )
         layout.label(text="")
-        layout.label(text="16일 이후 에이블러를 최신 버전으로 업데이트 하시어 보다 안정적인 서비스를 이용하시기 바랍니다.")
+        layout.label(
+            text="After the update, the previous version will no longer be supported."
+        )
         layout.label(text="")
-        layout.label(text="감사합니다.")
+        layout.label(text="Thank you.")
         layout.label(text="")
 
         split = layout.split()
         col1 = split.column()
-        anchor = col1.operator("acon3d.anchor", text="업데이트 방법 보러가기 (클릭)", icon="URL")
+        anchor = col1.operator(
+            "acon3d.anchor", text="HOW TO UPDATE (click)", icon="URL"
+        )
         anchor.href = (
             "https://www.notion.so/acon3d/0-2-2-f634e13265ca44f0bae7ed081762ec59"
         )
