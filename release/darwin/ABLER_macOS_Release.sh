0cbe9a2b367d9fe4ad8050e6e3004ccbf88dbb30
Seonggu Kim <seonggu.de.kim@gmail.com>
2022-06-20 21:20:34 -1000

    번들링 스크립트 하나로 통일 & 경로 정리를 위한 애셋 병합

--- release/darwin/ABLER_macOS_Release.sh
+++ release/darwin/ABLER_macOS_Release.sh
@@ -1,4 +1,9 @@
 testing=false
+# arch_setting : false -> arm64, true -> x86_64
+arch_setting=false
+if uname -m | grep -q "x86_64"; then
+   arch_setting=true
+fi
 # Handle arguments.
 while [[ $# -gt 0 ]]; do
     key=$1
@@ -12,33 +17,33 @@ while [[ $# -gt 0 ]]; do
 done
 # get codesign cert from keychain
 _codesign_cert="$(security find-identity -v -p codesigning | grep "Developer ID Application: carpenstreet Inc." | awk '{print $2}')"
-# cd ~/Desktop/ACON3D/RND_aconBlender/launcher_abler
-# pyinstaller --icon=icon.icns --onefile --windowed --uac-admin AblerLauncher.py -y --argv-emulation
-cd ~/Desktop/ABLER/blender || exit
+
+# build AblerLauncher and ABLER
+cd ../.. || exit
 git submodule update --init --recursive
 cd ./launcher_qt || exit
 make
-#cmake -S ./launcher_qt -B ./launcher_qt/build
-#cmake --build ./launcher_qt/build --target ALL_BUILD --config Release
 cd ..
 make
 cd ./release/darwin || exit
-_mount_dir="/Users/sdkfile/Desktop/ABLER/build_darwin/bin"
+_mount_dir="../../../build_darwin/bin"
 if ! "${testing}"; then
     macdeployqt ${_mount_dir}/ABLER.app -verbose=3
 
     echo ; echo -n "bundling .dylib libraries"
     for f in $(find "${_mount_dir}/ABLER.app" -name "*.dylib"); do
         echo "fixing ${f}"
+        if "${arch_setting}"; then
+          dylibbundler -cd -b -x "${f}" -d "${_mount_dir}"/ABLER.app/Contents/Frameworks/ -p @executable_path/../Frameworks/ -s /usr/local/lib
+        fi
         dylibbundler -cd -b -x "${f}" -d "${_mount_dir}"/ABLER.app/Contents/Frameworks/ -p @executable_path/../Frameworks/ -s /opt/homebrew/lib
     done
 
-    python ~/Desktop/ABLER/ABLER-Misc/ABLER_installer_assets/macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/AblerLauncher /opt/homebrew/Cellar/qt/6.2.3_1/
-    python ~/Desktop/ABLER/ABLER-Misc/ABLER_installer_assets/macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/ABLER /opt/homebrew/Cellar/qt/6.2.3_1/
+    python ./macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/AblerLauncher /opt/homebrew/Cellar/qt/6.2.3_1/
+    python ./macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/ABLER /opt/homebrew/Cellar/qt/6.2.3_1/
     sh ./bundle.sh --source "${_mount_dir}" --dmg ~/Desktop/ABLER.dmg --bundle-id com.acon3d.abler.release --username global@acon3d.com --password "@keychain:altool-password" --codesign "${_codesign_cert}"
 else
     macdeployqt "${_mount_dir}"/ABLER.app
-    # python macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/AblerLauncher /opt/homebrew/Cellar/qt/6.1.3/
-    python ~/Desktop/ABLER/ABLER-Misc/ABLER_installer_assets/macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/ABLER /opt/homebrew/Cellar/qt/6.2.3_1/
+    python ./macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/ABLER /opt/homebrew/Cellar/qt/6.2.3_1/
     sh ./bundle.sh --source "${_mount_dir}" --dmg ~/Desktop/ABLER.dmg --bundle-id com.acon3d.abler.release --username global@acon3d.com --password "@keychain:altool-password" --codesign "${_codesign_cert}" --test
 fi
