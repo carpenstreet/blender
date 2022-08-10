57ac531ee76cf4c8bd145bc241b19108d8e91cf2
Youdiie <youjin@acon3d.com>
2022-08-10 18:52:08 +0900

    pixel 지정 후 open

--- release/scripts/startup/abler/lib/psd-export/photoshop-python-api.py
+++ release/scripts/startup/abler/lib/psd-export/photoshop-python-api.py
@@ -1,23 +1,28 @@
 from photoshop import Session
+import photoshop.api as psi
 import os
 
-dir_path = "C:/Users/master/Downloads/Coni's room"
+dir_path = "C:/Users/master/Desktop/Coni's room"
 file_lst = os.listdir(dir_path)
 
-psd_file = dir_path + "/" + os.path.basename(dir_path)
+psd_file = dir_path + "/" + os.path.basename(dir_path) + ".psd"
+app = psi.Application()
+doc = app.documents.add(4800,2700)
+doc.saveAs(psd_file, psi.PhotoshopSaveOptions(), True)
+doc.close()
 
-try:
-    with Session(action="new_document", auto_close=True) as ps:
-        desc = ps.ActionDescriptor
-        event_id = ps.app.charIDToTypeID("Plc ")  # `Plc` need one space in here.
+# try:
+with Session(psd_file, action="open",auto_close=True) as ps:
+    desc = ps.ActionDescriptor
+    event_id = ps.app.charIDToTypeID("Plc ")  # `Plc` need one space in here.
 
-        for file in reversed(file_lst):
-            file_path = dir_path + "/" + file
-            desc.putPath(ps.app.charIDToTypeID("null"), file_path)
-            ps.app.executeAction(ps.app.charIDToTypeID("Plc "), desc)
+    for file in reversed(file_lst):
+        file_path = dir_path + "/" + file
+        desc.putPath(ps.app.charIDToTypeID("null"), file_path)
+        ps.app.executeAction(ps.app.charIDToTypeID("Plc "), desc)
         
-        ps.active_document.saveAs(psd_file, ps.PhotoshopSaveOptions(), True)
+        ps.active_document.save()
 
-except Exception as e:
-    raise e
+# except Exception as e:
+#     raise e
 
