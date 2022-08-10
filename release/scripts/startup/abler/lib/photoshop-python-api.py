fc47b0cb78e33ecff67cf283f3b4a703d8f2ad19
Youdiie <youjin@acon3d.com>
2022-08-10 19:34:57 +0900

    resizeImage로 한번에 변경

--- release/scripts/startup/abler/lib/photoshop-python-api.py
+++ release/scripts/startup/abler/lib/photoshop-python-api.py
@@ -10,13 +10,9 @@ im = Image.open(dir_path + "/" + file_lst[0], 'r')
 width, height = im.size
 
 psd_file = dir_path + "/" + os.path.basename(dir_path) + ".psd"
-try:
-    app = psi.Application()
-    doc = app.documents.add(width, height)
-    doc.saveAs(psd_file, psi.PhotoshopSaveOptions(), True)
-    doc.close()
 
-    with Session(psd_file, action="open",auto_close=True) as ps:
+try:
+    with Session(psd_file, action="new_document",auto_close=True) as ps:
         desc = ps.ActionDescriptor
         event_id = ps.app.charIDToTypeID("Plc ")  # `Plc` need one space in here.
 
@@ -25,7 +21,8 @@ try:
             desc.putPath(ps.app.charIDToTypeID("null"), file_path)
             ps.app.executeAction(ps.app.charIDToTypeID("Plc "), desc)
             
-            ps.active_document.save()
+        ps.active_document.resizeImage(width,height)
+        ps.active_document.saveAs(psd_file, psi.PhotoshopSaveOptions(), True)
 
 except Exception as e:
     raise e
