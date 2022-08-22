8df7b8ba3ef8aac8762355c9fb5680cfbace9e85
JJong84 <wony@acon3d.com>
2022-08-22 14:42:41 +0900

    CHANGE regex in pyproject.toml to cover abler_default.py (not worked due to file size) REFORMAT some files in abler directory

--- release/scripts/startup/abler/lib/import_file.py
+++ release/scripts/startup/abler/lib/import_file.py
@@ -9,7 +9,7 @@ class AconImportHelper(ImportHelper):
         :return: 파일이 아니거나 파일이 없을 경우 False를 반환, 존재하고 파일일 경우 True를 반환
         """
         path = self.filepath
-        if path.endswith('/') or path.endswith('\\') or path.endswith('//'):
+        if path.endswith("/") or path.endswith("\\") or path.endswith("//"):
             return False
         elif not os.path.isfile(path):
             bpy.ops.acon3d.alert(
