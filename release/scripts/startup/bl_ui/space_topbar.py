e7f0ad36da9bcbf489058860a2b22977cb8dacd2
Youdiie <youjin@acon3d.com>
2022-09-23 13:02:09 +0900

    컨벤션을 위해 '...'을 다시 붙이기로 함 (https://www.notion.so/acon3d/Language-Korean-Menu-fd9fab5dfb8b4cc99b2850bf8b7ec0f6?d=53667b9e07d946b18803555627b926c2#f71109dd7836401db206b6f2bee6a81b)

--- release/scripts/startup/bl_ui/space_topbar.py
+++ release/scripts/startup/bl_ui/space_topbar.py
@@ -288,7 +288,7 @@ class TOPBAR_MT_file(Menu):
 
         layout.operator_context = 'INVOKE_AREA'
         layout.menu("TOPBAR_MT_file_new", text="New", icon='FILE_NEW')
-        layout.operator("acon3d.file_open", text="Open", icon='FILE_FOLDER')
+        layout.operator("acon3d.file_open", text="Open...", icon='FILE_FOLDER')
         layout.menu("TOPBAR_MT_ACON3D_open_recent_files")
         layout.operator("wm.revert_mainfile")
         layout.menu("TOPBAR_MT_file_recover")
