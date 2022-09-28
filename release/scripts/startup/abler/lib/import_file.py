7b1d2817df734665da3806e11793adf80e900ce8
Youdiie <youjin@acon3d.com>
2022-09-28 15:00:57 +0900

    extension에 . 빼기

--- release/scripts/startup/abler/lib/import_file.py
+++ release/scripts/startup/abler/lib/import_file.py
@@ -9,7 +9,7 @@ class AconImportHelper(ImportHelper):
         :return: 파일이 아니거나 파일이 없을 경우 False를 반환, 존재하고 파일일 경우 True를 반환
         """
         basename = self.filepath.rsplit(".")[0]
-        path = basename + extension
+        path = basename + "." + extension
         if not os.path.isfile(path):
             bpy.ops.acon3d.alert(
                 "INVOKE_DEFAULT",
