76ba2f66f6d355380259ea18835311156485a8b9
JJong84 <wony@acon3d.com>
2022-10-18 17:10:09 +0900

    Cherry Picked feature/skp-single-layer commits onto feature/group-navigation-parent-child

--- release/scripts/startup/abler/custom_properties.py
+++ release/scripts/startup/abler/custom_properties.py
@@ -19,6 +19,7 @@
 
 import bpy
 from math import radians
+from .lib.layers import get_first_collection_name_of_object
 from .lib import scenes, cameras, shadow, objects
 from .lib.materials import materials_handler
 from . import object_control
@@ -55,24 +56,80 @@ class CollectionLayerExcludeProperties(bpy.types.PropertyGroup):
         del bpy.types.Scene.l_exclude
 
     def updateLayerVis(self, context):
+        l_exclude = bpy.context.scene.l_exclude
         target_layer = bpy.data.collections[self.name]
-        for objs in target_layer.objects:
-            belonging_col_names = {
-                collection.name for collection in objs.users_collection
-            }
-            should_show = all(
-                layer.value
-                for layer in bpy.context.scene.l_exclude
-                if layer.name in belonging_col_names
-            )
 
-            objs.hide_viewport = not should_show
-            objs.hide_render = not should_show
+        visited = set()
+
+        # 시작 오브젝트의 부모 오브젝트의 상태
+        def get_parent_value(obj):
+            cur = obj.parent
+            while cur:
+                if cur.hide_viewport:
+                    return False
+                cur = cur.parent
+            return True
+
+        # 부모 오브젝트 off => 항상 off,  부모 오브젝트 on => 현재 값으로 결정
+        def update_objects(obj, value: bool):
+            if obj.name in visited:
+                return
+            visited.add(obj.name)
+
+            if value:
+                for l in l_exclude:
+                    if (
+                        l.name == get_first_collection_name_of_object(obj)
+                        and not l.value
+                    ):
+                        value = False
+                        break
+
+            obj.hide_viewport = not value
+            obj.hide_render = not value
+
+            for o in obj.children:
+                update_objects(o, value)
+
+        for obj in target_layer.objects:
+            value = get_parent_value(obj) and self.value
+            update_objects(obj, value)
 
     def updateLayerLock(self, context):
+        l_exclude = bpy.context.scene.l_exclude
         target_layer = bpy.data.collections[self.name]
-        for objs in target_layer.objects:
-            objs.hide_select = self.lock
+
+        visited = set()
+
+        # 시작 오브젝트의 부모 오브젝트의 상태
+        def get_parent_lock(obj):
+            cur = obj.parent
+            while cur:
+                if cur.hide_select:
+                    return True
+                cur = cur.parent
+            return False
+
+        # 부모 오브젝트 off => 항상 off,  부모 오브젝트 on => 현재 값으로 결정
+        def update_objects(obj, lock: bool):
+            if obj.name in visited:
+                return
+            visited.add(obj.name)
+
+            if not lock:
+                for l in l_exclude:
+                    if l.name == get_first_collection_name_of_object(obj) and l.lock:
+                        lock = True
+                        break
+
+            obj.hide_select = lock
+
+            for o in obj.children:
+                update_objects(o, lock)
+
+        for obj in target_layer.objects:
+            lock = get_parent_lock(obj) or self.lock
+            update_objects(obj, lock)
 
     name: bpy.props.StringProperty(name="Layer Name", default="")
 
