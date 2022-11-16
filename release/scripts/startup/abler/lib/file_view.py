6c9043a36e8f62bc3c3ed8a7682df08b463e5c86
JJong84 <wony@acon3d.com>
2022-11-16 15:06:22 +0900

    Apply black to release/scripts/abler

--- release/scripts/startup/abler/lib/file_view.py
+++ release/scripts/startup/abler/lib/file_view.py
@@ -10,4 +10,6 @@ def file_view_title(title_enum):
     bpy.context.window_manager.set_fileselect_title(title_enum=title_enum)
     yield
     # 바로 기본값으로 되돌리면 위쪽 변경사항이 반영되지 않은 채 파일 다이얼로그가 나타나는 문제가 있어서, 한 틱 뒤에 원상복구
-    bpy.app.timers.register(lambda: bpy.context.window_manager.set_fileselect_title(title_enum="DEFAULT"))
+    bpy.app.timers.register(
+        lambda: bpy.context.window_manager.set_fileselect_title(title_enum="DEFAULT")
+    )
