268c1cf0aa18e6ca3d4f422f07cb7cf8cb064649
Seonggu Kim <seonggu.de.kim@gmail.com>
2022-12-12 17:54:07 +0900

    코드 가독성 개선

--- release/scripts/startup/abler/export_tab/render_control.py
+++ release/scripts/startup/abler/export_tab/render_control.py
@@ -572,18 +572,18 @@ class Acon3dRenderHighQualityOperator(Acon3dRenderDirOperator):
 
         # bpy.data.materials가 전체 씬에 대해 적용되기 때문에
         # 렌더 씬마다 적용하기 위해 재질의 상태를 렌더하기 전 pre_render에서 적용
-        if render_type != "full":
+        if render_type == "full":
+            pass
+        elif render_type == "line":
             for mat in bpy.data.materials:  # scene
-                if render_type == "line":
-                    mat.blend_method = "OPAQUE"
-                    mat.shadow_method = "OPAQUE"
-                    if toonNode := mat.node_tree.nodes.get(
-                        "ACON_nodeGroup_combinedToon"
-                    ):
-                        toonNode.inputs[1].default_value = 0
-                        toonNode.inputs[3].default_value = 1
-                else:
-                    materials_handler.set_material_parameters_by_type(mat)
+                mat.blend_method = "OPAQUE"
+                mat.shadow_method = "OPAQUE"
+                if toonNode := mat.node_tree.nodes.get("ACON_nodeGroup_combinedToon"):
+                    toonNode.inputs[1].default_value = 0
+                    toonNode.inputs[3].default_value = 1
+        else:  # Shadow / Texture
+            for mat in bpy.data.materials:  # scene
+                materials_handler.set_material_parameters_by_type(mat)
 
     def post_render(self, dummy, dum):
         base_scene_name, scene, render_type = self.render_queue[0]
