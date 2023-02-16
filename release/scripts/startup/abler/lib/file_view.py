9594a5bc7e09f68667b5dee44d58c4be730c477e
Carol <hayeon004@gmail.com>
2023-02-16 14:46:14 +0900

    바다 코끼리 연산자 활용

--- release/scripts/startup/abler/lib/file_view.py
+++ release/scripts/startup/abler/lib/file_view.py
@@ -9,12 +9,12 @@ def file_view_title(title_enum):
     """
     # set_fileselect_title가 없어서 발생하는 에러로 인해 if문 추가
     # 해당 이슈 링크: https://www.notion.so/acon3d/file_view_title-set_fileselect_title-98b55d3116f942f69759f0d63dd7be35?pvs=4
-    if bpy.context.window_manager.set_fileselect_title:
-        bpy.context.window_manager.set_fileselect_title(title_enum=title_enum)
+    if set_fileselect_title := bpy.context.window_manager.set_fileselect_title:
+        set_fileselect_title(title_enum=title_enum)
         yield
         # 바로 기본값으로 되돌리면 위쪽 변경사항이 반영되지 않은 채 파일 다이얼로그가 나타나는 문제가 있어서, 한 틱 뒤에 원상복구
         bpy.app.timers.register(
-            lambda: bpy.context.window_manager.set_fileselect_title(
+            lambda: set_fileselect_title(
                 title_enum="DEFAULT"
             )
         )
