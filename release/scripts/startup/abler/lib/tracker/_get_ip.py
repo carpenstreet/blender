0d24299a244cf6244daf7925ef8d6d2b9fdb1b89
Youdiie <faengiman@gmail.com>
2023-03-09 11:29:01 +0900

    SWTASK-84 주석 업데이트

--- release/scripts/startup/abler/lib/tracker/_get_ip.py
+++ release/scripts/startup/abler/lib/tracker/_get_ip.py
@@ -4,7 +4,8 @@ from requests import get
 
 def get_ip() -> Optional[str]:
     try:
-        # https://www.ipify.org/에서 제공하는 API를 사용
+        # https://www.ipify.org/에서 제공하는 IPv4 / IPv6 Universal API를 사용
+        # 참조: https://carpenstreet.atlassian.net/browse/SWTASK-84
         return get("https://api64.ipify.org", timeout=3).text
     except ConnectionError:
         print("Unable to reach the ipify service")
