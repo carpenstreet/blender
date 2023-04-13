5873d8c14f98bf06ade65b42989a1a5d06610bf2
JJong84 <wony@acon3d.com>
2023-04-13 20:15:12 +0900

    REMOVE QT codesigning codes and FIX key name error

--- release/darwin/ABLER_macOS_Release.sh
+++ release/darwin/ABLER_macOS_Release.sh
@@ -42,21 +42,8 @@ if ! "${testing}"; then
         dylibbundler -cd -of -b -x "${f}" -d "${_mount_dir}"/ABLER.app/Contents/Frameworks/ -p @executable_path/../Frameworks/ -s /opt/homebrew/lib
     done
 
-    # dylibbundler가 제대로 작돋하지 않을 경우를 대비해서 아래와 같은 스크립트를 추가적으로 실행해줌 -> 링크는 https://github.com/arl/macdeployqtfix 참조
-    if "${arch_setting}"; then
-      # 가장 최신 qt 버전을 찾아서 실행
-      _qt_loc="$(for dir in /usr/local/Cellar/qt/*/; do basename "$dir"; done | sort --reverse | head -1)"
-      python ./macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/AblerLauncher /usr/local/Cellar/qt/"${_qt_loc}"/
-      python ./macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/ABLER /usr/local/Cellar/qt/"${_qt_loc}"/
-    else
-      # 가장 최신 qt 버전을 찾아서 실행
-      _qt_loc="$(for dir in /opt/homebrew/Cellar/qt/*/; do basename "$dir"; done | sort --reverse | head -1)"
-      python ./macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/AblerLauncher /opt/homebrew/Cellar/qt/"${_qt_loc}"/
-      python ./macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/ABLER /opt/homebrew/Cellar/qt/"${_qt_loc}"/
-    fi
-
     # bundle.sh 실행
-    sh ./bundle.sh --source "${_mount_dir}" --dmg ~/Desktop/ABLER.dmg --bundle-id com.acon3d.abler.release --username global@acon3d.com --password "@keychain:altool-passwords" --codesign "${_codesign_cert}"
+    sh ./bundle.sh --source "${_mount_dir}" --dmg ~/Desktop/ABLER.dmg --bundle-id com.acon3d.abler.release --username global@acon3d.com --password "@keychain:altool-password" --codesign "${_codesign_cert}"
 else
     # qt 기본 제공 번들러
     macdeployqt ${_mount_dir}/ABLER.app -verbose=3
@@ -70,19 +57,6 @@ else
         fi
         dylibbundler -cd -of -od -b -x "${f}" -d "${_mount_dir}"/ABLER.app/Contents/Frameworks/ -p @executable_path/../Frameworks/ -s /opt/homebrew/lib
     done
-
-    # dylibbundler가 제대로 작돋하지 않을 경우를 대비해서 아래와 같은 스크립트를 추가적으로 실행해줌 -> 링크는 https://github.com/arl/macdeployqtfix 참조
-    if "${arch_setting}"; then
-      # 가장 최신 qt 버전을 찾아서 실행
-      _qt_loc="$(for dir in /usr/local/Cellar/qt/*/; do basename "$dir"; done | sort --reverse | head -1)"
-      python ./macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/AblerLauncher /usr/local/Cellar/qt/"${_qt_loc}"/
-      python ./macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/ABLER /usr/local/Cellar/qt/"${_qt_loc}"/
-    else
-      # 가장 최신 qt 버전을 찾아서 실행
-      _qt_loc="$(for dir in /opt/homebrew/Cellar/qt/*/; do basename "$dir"; done | sort --reverse | head -1)"
-      python ./macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/AblerLauncher /opt/homebrew/Cellar/qt/"${_qt_loc}"/
-      python ./macdeployqtfix.py "${_mount_dir}"/ABLER.app/Contents/macOS/ABLER /opt/homebrew/Cellar/qt/"${_qt_loc}"/
-    fi
 
     # bundle.sh 실행
     sh ./bundle.sh --source "${_mount_dir}" --dmg ~/Desktop/ABLER.dmg --bundle-id com.acon3d.abler.release --username global@acon3d.com --password "@keychain:altool-password" --codesign "${_codesign_cert}" --test
